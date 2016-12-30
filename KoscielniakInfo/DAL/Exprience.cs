bcccfa63bbca45d6afa937915c98ad1bf75e57be
Adam <koscielniakadam@gmail.com>
2016-12-30 13:29:30 +0100

    Building a Expirience Class

---
+++ KoscielniakInfo/DAL/Exprience.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KoscielniakInfo.DAL
+{
+    public class Exprience
+    {
+        public string companyName { get; set; }
+        public string jobRole { get; set; }
+        public string jobPosition { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime? endDate { get; set; }
+        public string wikipediaCompanyName { get; set; }
+        public string jobDescription { get; set; }
+        
+    }
+}
